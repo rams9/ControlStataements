43f7b1019a66f275ccaa8f8d255b1c89329674ba
ramanjaneyulu miriyala <ram.miriyala117@live.com>
2018-03-04 12:42:27 +0530

    second project

---
+++ Conditional.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Conditional.aspx.cs" Inherits="Conditional" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+          <h1>Switch Example</h1>
+        <h5>Enter 1 to 5</h5>
+        <span>Lucky Number</span>
+    <asp:TextBox ID="enterLucky" runat="server"></asp:TextBox>
+        <asp:Button ID="btnStatus" runat="server" Text="Status" OnClick="btnStatus_Click" />
+        <h3 id="userStatus" runat="server"></h3>
+
+
+    </div>
+        <div>
+            <h1>Conditional Statements</h1>
+            <asp:DropDownList ID="ddlTaxInfo" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlTaxInfo_SelectedIndexChanged">
+                <asp:ListItem Value="0">--Select--</asp:ListItem>
+                <asp:ListItem Value="1">1 - 20k</asp:ListItem>
+                <asp:ListItem Value="2">20k - 40k</asp:ListItem>
+                <asp:ListItem Value="3">40k - 60k</asp:ListItem>
+                <asp:ListItem Value="4">60k - 100k</asp:ListItem>
+                <asp:ListItem Value="5">Above 100k</asp:ListItem>
+
+            </asp:DropDownList>
+            <h3 id="txtPercentage" runat="server"></h3>
+        </div>
+        <h1>For Loop Example</h1>
+        <span>Enter Number</span>
+        <asp:TextBox ID="mTable" runat="server"></asp:TextBox>
+        <asp:Button ID="getTable" runat="server" Text="GetTable" OnClick="getTable_Click" />
+        <div id="forLoop1" runat="server"></div>
+
+        <h1>While Loop Example</h1>
+        
+        <span>Enter Number 1 to 100</span>
+        <asp:TextBox ID="txtWhile" runat="server"></asp:TextBox>
+        <asp:Button ID="btnPrint" runat="server" Text="Print Numbers" OnClick="btnPrint_Click"  />
+        <div id="whileLoop1" runat="server"></div>
+        <h2>Foreach</h2>
+        <div id="aForEach" runat="server"></div>
+        <h1>Operators</h1>
+        <h1>
+            Type Catsing
+        </h1>
+        <h3>Implicit conversion</h3>
+      <p>// Implicit conversion. num long can</p>  
+<p>  // hold any value an int can hold, and more!</p>  
+<p>  int num = 2147483647;</p>  
+<p>  long bigNum = num;</p>  
+
+        <h3>Explicit conversion</h3>
+       <p>double x = 1234.7;</p> 
+       <p> int a;</p>
+       <p> // Cast double to int.</p>
+       <p> a = (int)x;</p>
+        <p>// Output: 1234</p>
+    </form>
+</body>
+</html>
---
+++ Conditional.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Conditional : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        forecah();
+    }
+
+    public void LuckyStatus (int number)
+    {
+        string result = "";
+        //Switch Example
+        switch(number)
+        {
+            case 1:
+                {
+                    result = "You are Hero";
+                    break;
+                }
+            case 2:
+                {
+                    result = "You are Lazy";
+                    break;
+                }
+            case 3:
+                {
+                    result = "You are Good Boy";
+                    break;
+                }
+            case 4:
+                {
+                    result = "You are Good Boy";
+                    break;
+                }
+            case 5:
+                {
+                    result = "You are Robot";
+                    break;
+                }
+            default :{
+                 result = "You are Human Beaing..";
+                    break;
+            }
+        }
+        userStatus.InnerText = result;
+    }
+    protected void btnStatus_Click(object sender, EventArgs e)
+    {
+
+        LuckyStatus(Convert.ToInt32(enterLucky.Text));
+    }
+    protected void ddlTaxInfo_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        int money = Convert.ToInt32(ddlTaxInfo.Text);
+        //Conditional statements if, else if
+        if (money > 0)
+        {
+            if (money == 1)
+            {
+                txtPercentage.InnerText = "you Need to Pay 5%";
+            }
+            else if (money == 2)
+            {
+                txtPercentage.InnerText = "you Need to Pay 10%";
+            }
+            else if (money == 3)
+            {
+                txtPercentage.InnerText = "you Need to Pay 15%";
+            }
+            else if (money == 4)
+            {
+                txtPercentage.InnerText = "you Need to Pay 20%";
+            }
+            else
+            {
+                txtPercentage.InnerText = "you Need to Pay 30%";
+            }
+        }
+
+        else
+        {
+            txtPercentage.InnerText = "Select Any ";
+        }
+    }
+
+    public void forLoop(int j)
+    {
+        
+        string ptable="";
+     for(int i=1; i<=10 ; i++)
+     {
+         ptable = ptable+j+"*"+i+"="+(i*j)+"<br />";
+
+     }
+     forLoop1.InnerHtml = ptable;
+    }
+    public void whileLoop(int j)
+    { 
+        if(j<100 && j>0)
+        {
+            string ptable="";
+           while(j>=0)
+           {
+               ptable = ptable+j + "<br />";
+               j = j-1;
+           }
+           whileLoop1.InnerHtml = ptable;
+        }
+        else
+        {
+            dowhile(j);
+        }
+
+    }
+    protected void getTable_Click(object sender, EventArgs e)
+    {
+        if(Convert.ToInt32(mTable.Text)>0)
+        { 
+        forLoop(Convert.ToInt32(mTable.Text));
+        }
+        else
+        {
+            forLoop1.InnerHtml = "Enter Number >0";
+        }
+    }
+    protected void btnPrint_Click(object sender, EventArgs e)
+    {
+        whileLoop(Convert.ToInt32(txtWhile.Text));
+    }
+    public void dowhile (int j)
+    {
+       string ptable="";
+       do
+       {
+           ptable = ptable + j + "<br />";
+           j=j-1;
+       }
+       while (j > 100 && j <= 110);
+      
+       whileLoop1.InnerHtml = ptable;
+    }
+
+   public void forecah()
+    {
+        var values = "";
+
+        int[] a = new int[] {10,20,30,4,5,6};  
+
+        foreach(int element in a)
+        { 
+        
+            values = values +" "+ element;
+        }
+
+        aForEach.InnerText = values.ToString();
+    }
+}
